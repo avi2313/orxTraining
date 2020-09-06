cef5c83953f0d8adbcb630c68329db3daf31a70e
Your Name <you@example.com>
2020-09-06 15:24:38 +0300

    avi34

---
+++ src/components/NavBar.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components"
+
+function Navbar() {
+    const Bar = styled.div`
+        width:10em;
+        display: flex;
+        flex-direction: column;
+        align-items: flex-start;
+        justify-content: space-between;
+        flex: 1;
+      `
+
+    return (
+        <Bar>
+            <Link to="/">Home </Link>
+            <Link to="/about">About Us </Link>
+            <Link to="/shop">Shop Now </Link>
+        </Bar>
+    );
+};
+
+export default Navbar;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import React, { Component } from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import NavBar from './components/NavBar';
+import PageNotFound from './components/PageNotFound';
+import { Route, Switch } from "react-router-dom"
 
 ReactDOM.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
